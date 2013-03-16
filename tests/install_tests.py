39148e97a10357a1c72a2c2e218fb3e462135669
Sebastian Vetter <sebastian@roadside-developer.com>
2013-03-16 22:43:36 +1100

    Add test for successful installation of node

---
+++ tests/install_tests.py
@@ -0,0 +1,34 @@
+import sys
+import logging
+import subprocess
+
+sys.path.insert(0, '..')
+
+from unittest2 import TestCase
+
+
+class MockLoggingHandler(logging.Handler):
+    """Mock logging handler to check for expected logs."""
+
+    def __init__(self, *args, **kwargs):
+        self.reset()
+        logging.Handler.__init__(self, *args, **kwargs)
+
+    def emit(self, record):
+        self.messages[record.levelname.lower()].append(record.getMessage())
+
+    def reset(self):
+        self.messages = {
+            'debug': [],
+            'info': [],
+            'warning': [],
+            'error': [],
+            'critical': [],
+        }
+
+
+class TestNode(TestCase):
+
+    def test_is_installed(self):
+        output = subprocess.check_output(['node', '--version'])
+        self.assertEquals(output.strip(), 'v0.8.11')
